6907634b237cfd88d34055ec38f101c333d3b7be
takashi-kozai <129640872+takashi-kozai@users.noreply.github.com>
2023-05-13 18:33:39 +0900

    DEBUG コードを追加

--- Lesson 5 Digital Inputs/Digital_Inputs/Digital_Inputs.ino
+++ Lesson 5 Digital Inputs/Digital_Inputs/Digital_Inputs.ino
@@ -1,6 +1,15 @@
 //www.elegoo.com
 //2016.12.08
 
+// DEBUG 用のマクロ
+//#define DEBUG
+#ifdef DEBUG
+#define DBG(x) x
+#else
+#define DBG(x)
+#endif
+
+
 int ledPin = 5;
 int buttonApin = 9;
 int buttonBpin = 8;
@@ -11,17 +20,28 @@ void setup()
 {
   pinMode(ledPin, OUTPUT);
   pinMode(buttonApin, INPUT_PULLUP);  
-  pinMode(buttonBpin, INPUT_PULLUP);  
+  pinMode(buttonBpin, INPUT_PULLUP); 
+
+  // open serial port for debug;
+  DBG(Serial.begin(9600);)
 }
 
 void loop() 
 {
+  DBG(Serial.println("start");)
+
   if (digitalRead(buttonApin) == LOW)
   {
+    DBG(Serial.println("buttonApin low");)
     digitalWrite(ledPin, HIGH);
   }
   if (digitalRead(buttonBpin) == LOW)
   {
+    DBG(Serial.println("buttonBpin low");)
     digitalWrite(ledPin, LOW);
   }
+
+  DBG(Serial.println("end");)
+  DBG(delay(1000);)
+
 }
