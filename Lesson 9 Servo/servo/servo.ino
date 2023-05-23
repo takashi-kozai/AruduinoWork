d31ca4a1d059a52457a3cce8664cd87db2115073
takashi-kozai <129640872+takashi-kozai@users.noreply.github.com>
2023-05-23 23:08:27 +0900

    Servo の実験

--- Lesson 9 Servo/servo/servo.ino
+++ Lesson 9 Servo/servo/servo.ino
@@ -20,7 +20,7 @@ void loop() {
   }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(5);                       // waits 15ms for the servo to reach the position
   }
  
 }
